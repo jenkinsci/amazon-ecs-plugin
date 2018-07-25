c45840160bf94dae9e839ac62a53ce692a4ea699
Philipp Garbe <philipp.garbe@web.de>
2018-07-25 16:56:16 +0200

    Use built-in OnceRetentionStrategy

--- src/main/java/com/cloudbees/jenkins/plugins/amazonecs/ECSSlave.java
+++ src/main/java/com/cloudbees/jenkins/plugins/amazonecs/ECSSlave.java
@@ -27,22 +27,17 @@ package com.cloudbees.jenkins.plugins.amazonecs;
 
 import java.io.IOException;
 import java.util.Collections;
-
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import javax.annotation.concurrent.GuardedBy;
+
+import org.jenkinsci.plugins.durabletask.executors.OnceRetentionStrategy;
 
 import hudson.model.Descriptor;
 import hudson.model.TaskListener;
-import hudson.node_monitors.ResponseTimeMonitor;
 import hudson.slaves.AbstractCloudComputer;
 import hudson.slaves.AbstractCloudSlave;
 import hudson.slaves.ComputerLauncher;
-import hudson.slaves.RetentionStrategy;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * This slave should only handle a single task and then be shutdown.
@@ -51,7 +46,7 @@ import java.util.logging.Logger;
  */
 public class ECSSlave extends AbstractCloudSlave {
 
-    private static final Logger LOGGER = Logger.getLogger(ECSCloud.class.getName());
+    private static final long serialVersionUID = 1;
 
     @Nonnull
     private final ECSCloud cloud;
@@ -70,63 +65,12 @@ public class ECSSlave extends AbstractCloudSlave {
     @CheckForNull
     private String taskArn;
 
-    private static RetentionStrategy deleteAfterFinished = new RetentionStrategy<ECSComputer>() {
-        @Override
-        public boolean isManualLaunchAllowed(ECSComputer c) {
-            return false;
-        }
-
-        @Override
-        @GuardedBy("hudson.model.Queue.lock")
-        public long check(ECSComputer c) {
-            LOGGER.log(Level.FINE, "Checking computer: {0}", c);
-
-            AbstractCloudSlave node = c.getNode();
-
-            // If the computer is NOT idle, then it is currently running some task.
-            // In this case, we are going to tell Jenkins that it can no longer accept
-            // any new tasks, which will cause it to create a new node for any subsequent
-            // tasks.
-            if(!c.isIdle() ) {
-                LOGGER.log(Level.FINE, "Computer is not idle; setting it to no longer accept tasks.");
-                c.setAcceptingTasks( false );
-            }
-
-            // If the computer IS idle AND it is no longer accepting tasks, then it has
-            // already had a task and completed it.  In this case, we are going to terminate
-            // the node.
-            if(c.isIdle() && !c.isAcceptingTasks() && node != null) {
-                LOGGER.log(Level.FINE, "Computer is idle and not accepting tasks; terminating it.");
-                try {
-                    node.terminate();
-                } catch (InterruptedException e) {
-                    LOGGER.log(Level.WARNING, "Failed to terminate " + c.getName(), e);
-                } catch (IOException e) {
-                    LOGGER.log(Level.WARNING, "Failed to terminate " + c.getName(), e);
-                }
-            }
-
-            // If the Response Time Monitor has marked this computer as not responding, then
-            // we are going to terminate the node to free up resources.
-            if (c.getOfflineCause() instanceof ResponseTimeMonitor.Data && node != null) {
-                LOGGER.log(Level.FINE, "Computer is not responding; terminating it");
-                try {
-                    node.terminate();
-                } catch (InterruptedException e) {
-                    LOGGER.log(Level.WARNING, "Failed to terminate " + c.getName(), e);
-                } catch (IOException e) {
-                    LOGGER.log(Level.WARNING, "Failed to terminate " + c.getName(), e);
-                }
-            }
-
-            // Tell Jenkins to check again in 1 minute.
-            return 1;
-        }
-
-    };
-
-    public ECSSlave(@Nonnull ECSCloud cloud, @Nonnull String name, @Nullable String remoteFS, @Nullable String labelString, @Nonnull ComputerLauncher launcher) throws Descriptor.FormException, IOException {
-        super(name, "ECS slave", remoteFS, 1, Mode.EXCLUSIVE, labelString, launcher, deleteAfterFinished, Collections.EMPTY_LIST);
+    public ECSSlave(@Nonnull ECSCloud cloud, 
+                    @Nonnull String name, 
+                    @Nullable String remoteFS, 
+                    @Nullable String labelString, 
+                    @Nonnull ComputerLauncher launcher) throws Descriptor.FormException, IOException {
+        super(name, "ECS slave", remoteFS, 1, Mode.EXCLUSIVE, labelString, launcher, new OnceRetentionStrategy(5), Collections.emptyList());
         this.cloud = cloud;
     }
 
@@ -155,7 +99,7 @@ public class ECSSlave extends AbstractCloudSlave {
     }
 
     @Override
-    public AbstractCloudComputer createComputer() {
+    public AbstractCloudComputer<ECSSlave> createComputer() {
         return new ECSComputer(this);
     }
 
@@ -164,6 +108,11 @@ public class ECSSlave extends AbstractCloudSlave {
         if (taskArn != null) {
             cloud.deleteTask(taskArn, clusterArn);
         }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("ECSSlave name: %s", name);
     }
 
     public ECSCloud getCloud() {
